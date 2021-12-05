68b4071f70b98d21a17cfd2bad36f0b3e42cb1f4
zypeh <zypeh@users.noreply.github.com>
2021-12-05 21:20:36 +0800

    Added part2 for day2.hs

--- day2.hs
+++ day2.hs
@@ -22,20 +22,27 @@ data Command
 data Pos = Pos Int Int
 
 instance Show Pos where
-  show (Pos x y) = "The final result is: " ++ show (x * y)
+  show (Pos x y) = show (x * y)
 
 main :: IO ()
 main = do
   args <- getArgs
   src <- readFile (head $ tail args)
   let input = fromRight [] $ P.parse' (many $ parseLog <* P.char8 '\n') src
-  print $ go (Pos 0 0) input
+  putStrLn $ "Part 1 is " <> show (go (Pos 0 0) input)
+  putStrLn $ "Part 2 is " <> show (goAim (Pos 0 0, 0) input)
 
 go :: Pos -> [Command] -> Pos
 go pos [] = pos
 go (Pos x y) ((Forward n):rest) = go (Pos (x + n) y) rest
 go (Pos x y) ((Down n):rest) = go (Pos x (y + n)) rest
 go (Pos x y) ((Up n): rest) = go (Pos x (y - n)) rest
+
+goAim :: (Pos, Int) -> [Command] -> Pos
+goAim (pos, _) [] = pos
+goAim (Pos x y, a) ((Forward n):rest) = goAim (Pos (x + n) (y + (a * n)), a) rest
+goAim (Pos x y, a) ((Down n):rest) = goAim (Pos x y, a + n) rest
+goAim (Pos x y, a) ((Up n): rest) = goAim (Pos x y, a - n) rest
 
 parseLog :: Parser Command
 parseLog = do
