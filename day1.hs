4bc73c361add9a1486774788e775166545c3bfc9
zypeh <zypeh@users.noreply.github.com>
2021-12-05 18:58:49 +0800

    Added part2 for day1.hs

--- day1.hs
+++ day1.hs
@@ -3,7 +3,7 @@ module Main where
 
 import Prelude hiding (readFile)
 import Z.IO.FileSystem.Base (readFile)
-import Z.IO (getArgs)
+import Z.IO (getArgs, printStdLn)
 
 import qualified Z.Data.Parser as P
 import Control.Applicative (many)
@@ -14,7 +14,8 @@ main = do
   args <- getArgs
   src <- readFile (head $ tail args)
   let input = fromRight [] $ P.parse' (many $ P.int <* P.char8 '\n') src
-  print $ sonar input
+  putStrLn $ "part1: " <> show (sonar input)
+  putStrLn $ "part2: " <> show (sonar' input)
 
 sonar :: [Int] -> Int
 sonar [] = 0
@@ -23,3 +24,9 @@ sonar (x:xs) = go (0, x) xs
     go :: (Int, Int) -> [Int] -> Int
     go (counter, _) [] = counter
     go (counter, prev) (curr:rest) = go (if curr > prev then counter + 1 else counter, curr) rest
+
+sonar' :: [Int] -> Int
+sonar' input = sonar $ (\(a, b, c) -> a + b + c) <$> zip3 input input' input''
+  where
+    input' = tail input
+    input'' = tail input'
